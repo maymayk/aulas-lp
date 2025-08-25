742d2b5bab1cfed9135423a4c1814769639968e1
May Uni <mayuniilkim@gmail.com>
2025-08-25 10:27:11 -0300

    exercício 1 da Lista 3

---
+++ Lista3/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int contador = 1;
+        float nota;
+        Scanner sc = new Scanner(System.in);
+        float soma = 0;
+
+        while (contador <=5){
+            System.out.println("Informe a nota " + contador);
+            nota = sc.nextFloat();
+            soma += nota;
+            contador++;
+        }
+        float media = soma/5;
+        System.out.println("A média das notas é: " + media);
+    }
+}
